4cc7d75fac25ff402b3e4a78217bbf518f19b467
Guddu Kumar <kumarguddu609@gmail.com>
2024-03-01 12:46:41 +0530

    app

---
+++ server.js
@@ -0,0 +1,25 @@
+const SMTPServer = require("smtp-server").SMTPServer;
+const server = new SMTPServer({
+  allowInsecureAuth: true,
+  authOptional: true,
+  onConnect(session, cb) {
+    console.log(`onConnect`, session.id);
+    cb(); //Accept the connection
+  },
+  onMailFrom(address, session, cb) {
+    console.log(`onMailFrom`, address.address, cb);
+    cb();
+  },
+  onRcptTo(address, session, cb) {
+    console.log(`onRcptTo`, address.address, session.id);
+    cb();
+  },
+  onData(stream, session, cb) {
+    stream.on("data", (data) => console.log(`onData ${data.toString()}`));
+    stream.on("end", cb);
+  },
+});
+
+server.listen(25, () => {
+  console.log("listening");
+});
